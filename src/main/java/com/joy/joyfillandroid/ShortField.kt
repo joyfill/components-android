9ef6a316f679b44070619c466f07490fee4d1e6e
Ayush <ayushb041982@gmail.com>
2023-08-03 18:40:37 +0530

    Issue #no-46 onDraw has been changed with globalize class

--- src/main/java/com/joy/joyfillandroid/ShortField.kt
+++ src/main/java/com/joy/joyfillandroid/ShortField.kt
@@ -16,7 +16,7 @@ class ShortField(context: Context) : EditText(context) {
 
     init {
         val textField = TextField(context)
-        val linearLayout = LinearLayout(getContext())
+        val linearLayout = LinearLayout(context)
         // Apply custom design modifications
         val drawable = ContextCompat.getDrawable(context, R.drawable.custom_background)
         background = drawable
@@ -52,26 +52,13 @@ class ShortField(context: Context) : EditText(context) {
 
     }
 
-
+    // Perform actions when the text changes
     override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter)
-
-        // Perform actions when the text changes
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
-        val linearLayout = LinearLayout(context)
-        val layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        layoutParams.setMargins(30,10,30,30)
-        setLayoutParams(layoutParams)
-        layoutParams.gravity = Gravity.CENTER_VERTICAL
-        linearLayout.layoutParams = layoutParams
-        linearLayout.orientation = LinearLayout.VERTICAL
+        OnDrawHelper.onDrawGlobal(context, this)
     }
-
 }
