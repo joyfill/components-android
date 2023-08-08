495ccdc87e4315d7144505326d1c4bff49cf2975
Ayush <ayushb041982@gmail.com>
2023-08-08 17:24:18 +0530

    Issue #no-46 changes updated

--- src/main/java/com/joy/joyfillandroid/ShortField.kt
+++ src/main/java/com/joy/joyfillandroid/ShortField.kt
@@ -1,6 +1,5 @@
 package com.joy.joyfillandroid
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -10,55 +9,49 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 
-@SuppressLint("AppCompatCustomView", "ResourceAsColor")
-class ShortField(context: Context) : EditText(context) {
-
+class ShortField(context: Context): LinearLayout(context) {
+    private var shortTextTitle = ""
+    private var shortTextValue = ""
+    val textField = TextField(context)
+    private val editText = EditText(context)
 
     init {
-        val textField = TextField(context)
-        val linearLayout = LinearLayout(context)
-        // Apply custom design modifications
+        orientation = VERTICAL
+        gravity = Gravity.START
+        textField.text = shortTextTitle
+        addView(textField)
+
         val drawable = ContextCompat.getDrawable(context, R.drawable.custom_background)
-        background = drawable
+        editText.background = drawable
 
         // set Padding
         val padding = resources.getDimensionPixelSize(R.dimen.custom_edittext_padding)
-        setPadding(padding, padding, padding, padding)
+        editText.setPadding(padding, padding, padding, padding)
         val  margin = resources.getDimensionPixelSize(R.dimen.custom_edittext_padding)
         val layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
-        linearLayout.layoutParams = layoutParams
-        //set margins
-        layoutParams.setMargins(margin, margin, margin, margin)
-
-        setText("Jane Doe")
-        textSize = 14f
-        setTextColor(Color.BLACK)
+        layoutParams.setMargins(dpToPx(context,14), dpToPx(context,5), dpToPx(context,14), dpToPx(context,14))
+        editText.layoutParams = layoutParams
+        editText.setText(shortTextValue)
+        editText.textSize = 14f
+        editText.setTextColor(Color.BLACK)
         val inputType = InputType.TYPE_CLASS_TEXT
-        setInputType(inputType)
+        editText.setInputType(inputType)
         // set Single Line
-        setSingleLine(true)
 
-        textField.text = "Text Field"
-        linearLayout.addView(textField)
-        addView(linearLayout)
-    }
-
-    private fun addView(linearLayout: LinearLayout) {
-    val parentView = LinearLayout(context)
-    parentView.addView(linearLayout)
-
-    }
-
-    // Perform actions when the text changes
-    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
-        super.onTextChanged(text, start, lengthBefore, lengthAfter)
+        editText.setSingleLine(true)
+        addView(editText)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
         OnDrawHelper.onDrawGlobal(context, this)
+        super.onDraw(canvas)
+    }
+
+    fun updateTextFieldTitle(title: String, value: String) {
+        textField.text = title
+        editText.setText(value)
     }
 }
